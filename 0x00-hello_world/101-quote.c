85e9d12fe1509d644a68e287a78c5e9a47c43c19
Anthonytrisha <anthonytrisha04@gmail.com>
2022-09-10 07:34:57 -0700

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,11 @@
  *
  *Return: end program
  */
+
 int main(void)
 {
-	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fprintf(stderr, "%s\n", n);
-	return (1);
+		char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+			fprintf(stderr, "%s\n", n);
+	
+			return (1);
 }
