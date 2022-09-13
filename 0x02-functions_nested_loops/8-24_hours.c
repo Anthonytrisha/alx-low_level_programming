ec1f4f87e0eac711b6e10a547e48ed8e98f51f91
Anthonytrisha <anthonytrisha04@gmail.com>
2022-09-13 14:27:57 -0700

    I am federal agentJack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,15 @@ void jack_bauer(void)
 		h2 = hour % 10;
 		for (min = 0; min < 60; ++min)
 		{
-			h1 = hour / 10;
-			h2 = hour % 10;
-			for (min = 0; min < 60; ++min)
-			{
-				m1 = min / 10;
-				m2 = min % 10;
-				_putchar(h1 + '0');
-				_putchar(h2 + '0');
-				_putchar(':');
-				_putchar(m1 + '0');
-				_putchar(m2 + '0');
-				_putchar(10);
-			}
-			min = 0;
+			m1 = min / 10;
+			m2 = min % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar(10);
 		}
+		min = 0;
 	}
+}
