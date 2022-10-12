02691e4b77df9444c7102dc368ce74633d707516
Anthonytrisha <anthonytrisha04@gmail.com>
2022-10-12 12:09:56 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * op_add - addition
- * @a: int 2
- * @b: int 1
- * Return: a + b
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: add.
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - subrtact
- * @a: integer
- * @b: integer
- * Return: a - b
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: difference.
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - multiply
- * @a: integer
- * @b: integer
- * Return: result
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
  */
+
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
+
 /**
- * op_div - division
- * @a: integer
- * @b: integer
- * Return: divison
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: division.
  */
+
 int op_div(int a, int b)
 {
-return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
+
 /**
- * op_mod - modulo function
- * @a: integer
- * @b: integer
- * Return: a mod b
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: remainder of the division
  */
+
 int op_mod(int a, int b)
 {
-return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
