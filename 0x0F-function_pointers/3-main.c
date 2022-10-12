e485b54459814f2780bfeea80370c539b4e04ac7
Anthonytrisha <anthonytrisha04@gmail.com>
2022-10-12 11:45:17 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
-#include "3-calc.h"
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * main - Entry point of program
- * @argc: size of array argv and number of command line arguments
- * @argv: An array of size argc
- * Return: Always successful
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
+ * Description: calculates basic arithmetic
+ * Return: 0 on success, 98 on failure
  */
 
 int main(int argc, char *argv[])
 {
-	int (*operator)(int, int)
-		int num1, num2;
+	char op;
+	int (*op_func)(int, int);
+	int result;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	operator = get_op_func(argv[2]);
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	if (operator == NULL)
-	{
+	op = *(argv[2]);
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{/* divide by zero exception */
 		printf("Error\n");
-		exit(99);
+		return (100);
 	}
-
-	if ((argv[2][0] == '%' || argv[2][0] == '/') && atoi(argv[3]) == 0)
-
-		return (0);
+	/* actually do the operation now that we've error chcked */
+	op_func = get_op_func(&op);
+	if (op_func == NULL || argv[2][1] != '\0')
+	{ /* didn't find operator, or operator longer than 1 byte */
+		printf("Error\n");
+		return (99);
+	}
+	result = op_func(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
 }
