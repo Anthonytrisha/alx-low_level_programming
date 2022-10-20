2a74ae8a2a3f656fdcfaa634dc0f522b69c834a2
Anthonytrisha <anthonytrisha04@gmail.com>
2022-10-20 12:33:28 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * bmain - function executed before main
- * Return: no return.
+ * myCleanupFun - function that prints somthing before
+ * the main function is executed.
  */
+void myCleanupFun(void) __attribute__ ((constructor));
 
-void __attribute__ ((constructor)) bmain()
+void myCleanupFun(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
